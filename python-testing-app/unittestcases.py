8df1eb5f19098b2fcf2fbe5f5cddcdca9bad0d3c
raviteja4421 <ravitejadaggu@gmail.com>
2024-09-21 11:26:22 +0530

    Update unittestcases.py

--- python-testing-app/unittestcases.py
+++ python-testing-app/unittestcases.py
@@ -19,5 +19,18 @@ class multiplyTestcase(unittest,testcase):
 
      self.assertEqual(result, 12)
 
+    
+   def test_4(self):
+
+     result = multiplication(-3,4)
+
+     self.assertEqual(result, 22)
+
+   def test_5(self):
+
+     result = multiplication(-1,-1)
+
+     self.assertEqual(result, 1)
+
 if __name__ == '__main__'
     unittest.main()
